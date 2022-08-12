d42e535f45043e2a0b9ebc5069a0914b9ec8822c
TasosTsiam <mfs170lvl@hotmail.com>
2022-08-12 19:30:51 +0300

    Email validation on login section is ready.

--- Main/login/myjs/login.js
+++ Main/login/myjs/login.js
@@ -1,9 +1,19 @@
-function validate()
-{
-    var username=document.getElementById("email").value;
-    var password=document.getElementById("psw").value;
-    if (!username.includes("@"))
-    {
-        alert("Invalid Email / Password");
+var email = document.getElementById("email");
+var password = document.getElementById("psw");
+var logErrorMsg = document.getElementById("login-error-msg");
+var loginButton = document.getElementById("login_button");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    var regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if (email.value.match(regEx)) {
+        alert("That's a gucci email, champ! Aye!")
+        return true
     }
-}
+    else {
+        logErrorMsg.style.opacity = 1;
+        return false;
+    }
+    
+})
