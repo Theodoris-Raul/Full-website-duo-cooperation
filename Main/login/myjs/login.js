cf022c0f56daf727b9b4dedf3e13743ddc927f4d
TasosTsiam <mfs170lvl@hotmail.com>
2022-08-12 21:00:28 +0300

    2 more hover functions added in JS file.

--- Main/login/myjs/login.js
+++ Main/login/myjs/login.js
@@ -3,18 +3,30 @@ var password = document.getElementById("psw");
 var logErrorMsg = document.getElementById("login-error-msg");
 var logSuccessMsg = document.getElementById("login-success-msg");
 var loginButton = document.getElementById("login_button");
+var email_val_suggestion = document.getElementById("email_validation")
+var password_val_suggestion = document.getElementById("password_validation")
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
 
     var regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    if (email.value.match(regEx)) {
+    var regEx2 = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/
+    if (email.value.match(regEx) && password.value.match(regEx2)) {
         logSuccessMsg.style.opacity = 1;
         return true
     }
     else {
         logErrorMsg.style.opacity = 1;
         return false;
-    }
-    
+    }    
 })
+
+password.addEventListener("mouseover", (e) => {
+    password_val_suggestion.style.opacity = 1;
+}
+)
+
+password.addEventListener("mouseout", (e) => {
+    password_val_suggestion.style.opacity = 0;
+}
+)
