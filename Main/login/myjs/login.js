0dc26278cbe09a427342bf76283cdf00f6c6afb2
TasosTsiam <mfs170lvl@hotmail.com>
2022-08-17 15:53:41 +0300

    Lots of changes, JQuery involvement, login done.

--- Main/login/myjs/login.js
+++ Main/login/myjs/login.js
@@ -1,10 +1,7 @@
 var email = document.getElementById("email");
 var password = document.getElementById("psw");
-var logErrorMsg = document.getElementById("login-error-msg");
-var logSuccessMsg = document.getElementById("login-success-msg");
 var loginButton = document.getElementById("login_button");
 var email_val_suggestion = document.getElementById("email_validation")
-var password_val_suggestion = document.getElementById("password_validation")
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -12,21 +9,41 @@ loginButton.addEventListener("click", (e) => {
     var regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     var regEx2 = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/
     if (email.value.match(regEx) && password.value.match(regEx2)) {
-        logSuccessMsg.style.opacity = 1;
-        return true
+        $(document).ready(function(){
+            $("#login-success-msg").show();
+            setTimeout(function() { $("#login-success-msg").hide(); }, 2000);
+            
+        })
+    }
+    else if (email.value.length == 0) {
+        $(document).ready(function(){
+            $("#required_email").show();
+            setTimeout(function() { $("#required_email").hide(); }, 2000);
+        })
+    }
+    else if (password.value.length == 0) {
+        $(document).ready(function(){
+            $("#required_psw").show();
+            setTimeout(function() { $("#required_psw").hide(); }, 2000);
+        })
     }
     else {
-        logErrorMsg.style.opacity = 1;
-        return false;
+        $(document).ready(function(){
+            $("#login-error-msg").show();
+            setTimeout(function() { $("#login-error-msg").hide(); }, 2000);
+            
+        })
     }    
 })
 
-password.addEventListener("mouseover", (e) => {
-    password_val_suggestion.style.opacity = 1;
-}
-)
+$(document).ready(function(){
+    $("#psw").mouseover(function(){
+      $("#password_validation").show();
+    });
+  });
 
-password.addEventListener("mouseout", (e) => {
-    password_val_suggestion.style.opacity = 0;
-}
-)
+$(document).ready(function(){
+$("#psw").mouseout(function(){
+    $("#password_validation").hide();
+});
+});
