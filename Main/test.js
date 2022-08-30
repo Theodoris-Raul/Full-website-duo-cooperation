7ad7c00ec28b1f2d52765446b4c369c0ba131ffb
Theodoris-Raul <theodorostsiamitas@gmail.com>
2022-08-30 14:19:42 +0300

    rrrriiiiiiiiii

--- Main/test.js
+++ Main/test.js
@@ -7,5 +7,29 @@ for (but of button)
     var add = Number(noti.getAttribute('data-count')|| 0);
     noti.setAttribute('data-count', add +1);
     noti.classList.add('zero');
+
+    // copy and paste element //
+
+    var parent = e.target.parentNode;
+    var clone = parent.cloneNode(true);
+    select.appendChild(clone);
+    clone.lastElementChild.innerText = "Buy-now";
+    if (clone) {
+      noti.onclick =()=>{
+        select.classList.toggle('display');
+      }
+    }
+
+    // image animation to cart //
+
+    var image = e.target.parentNode.querySelector('img');
+    var span = e.target.parentNode.querySelector('span');
+    var s_image = image.cloneNode(false);
+    span.appendChild(s_image);
+    span.classList.add('active');
+    setTimeout(()=>{
+      span.removeChild(s_image);
+      span.classList.remove('active');
+    } ,500);
   })
 }
